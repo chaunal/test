1c88ef01bee02e165955d95bdfb6462b0c56b874
chaunal <chaunal@gmail.com>
2017-03-13 17:34:36 -0500

    Update constants.groovy

--- constants.groovy
+++ constants.groovy
@@ -1,1 +1,14 @@
 
+@Immutable
+class Coordinates {
+    String  a = "amit"
+    String   b = "sumit"
+
+    String  getA(int idx) {
+        a
+    }
+	
+	String  getb(int idx) {
+        b
+    }
+}
